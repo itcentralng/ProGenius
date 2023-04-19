6a1d166010050a1f19acef0ae7908993f6a4983e
Nasir Mustapha <teboy.com@gmail.com>
2023-04-19 15:12:43 +0100

    fix manage.py -> remove old content

--- manage.py
+++ manage.py
@@ -1,9 +1,5 @@
 from datetime import datetime
 from app import app, db
-from app.user.model import User
-from app.call.model import Call
 
 with app.app_context():
-    with app.app_context():
-        db.create_all()
-    print('Ready!')
+    pass
