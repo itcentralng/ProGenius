388ad32c27627c53ed13ddce3c3d40520594e14f
Nasir Mustapha <teboy.com@gmail.com>
2023-04-20 05:43:04 +0100

    add exception logging on tasks

--- app/celery/tasks.py
+++ app/celery/tasks.py
@@ -1,4 +1,5 @@
 from app import celery, db
+from app.image.schema import ImageSchema
 
 from app.note.model import Note
 from app.image.model import Image
@@ -30,12 +31,13 @@ def create_note_task(subject, topic, curriculum, level, user_id, transient_audio
         data = NoteSchema().dump(note)
 
         # emit the JSON data to the server
-        sio.emit('json', {'note':data})
+        sio.emit('note', {'note':data})
 
         # disconnect from the server
         sio.disconnect()
         return "Note Generated Successfully!"
-    except:
+    except Exception as e:
+        print(e)
         os.remove(transient_audio_file)
         db.session.rollback()
         return "Note Could Not Be Generated Successfully!"
@@ -43,8 +45,8 @@ def create_note_task(subject, topic, curriculum, level, user_id, transient_audio
 @celery.task
 def regenerate_image(image_id, prompt=None):
     try:
-        old_prompt = image.prompt
         image = Image.get_by_id(image_id)
+        old_prompt = image.prompt
         image.prompt = prompt or image.prompt
         encoded_string = dream(image.prompt)
         image_url = add(encoded_string)
@@ -58,14 +60,15 @@ def regenerate_image(image_id, prompt=None):
         # connect to the Socket.IO server
         sio.connect(os.environ.get('SOCKET_SERVER'))
         # create a JSON object
-        data = NoteSchema().dump(note)
+        data = ImageSchema().dump(image)
 
         # emit the JSON data to the server
-        sio.emit('json', {'note':data})
+        sio.emit('image', {'image':data, 'creator_id':note.creator_id})
 
         # disconnect from the server
         sio.disconnect()
         return "Image Generated Successfully!"
-    except:
+    except Exception as e:
+        print(e)
         db.session.rollback()
         return "Image Could Not Be Generated Successfully!"
