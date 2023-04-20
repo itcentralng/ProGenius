7c27bd81d7881fcb7be7ae320458eb5cb3b0ed70
Nasir Mustapha <teboy.com@gmail.com>
2023-04-20 13:14:04 +0100

    add db pool_pre_ping

--- config/db.py
+++ config/db.py
@@ -2,3 +2,4 @@ import os
 # Database configuration
 SQLALCHEMY_DATABASE_URI= os.environ.get('DATABASE_URI', 'sqlite:///app.db')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
+SQLALCHEMY_ENGINE_OPTIONS = {"pool_pre_ping": True}
